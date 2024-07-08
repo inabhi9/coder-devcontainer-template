a0498ecd0e3f5189b8b0b4ae05cc2b7eb667154a
Abhinav <kotak@ldopa.in>
2024-07-08 20:10:52 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-cd /workspaces
-mkdir -p .vscode-cli .docker /root/.docker
+mkdir -p /workspaces/.vscode-cli /workspaces/.docker ~/.ssh ~/.logs
+sudo mkdir -p /root/.docker
 
 # Found this api when these CODER_AGENT_* env vars are not set and executing `./coder gitssh --`
 curl -s -H "Coder-Session-Token: $CODER_AGENT_TOKEN" ${CODER_AGENT_URL}api/v2/workspaceagents/me/gitsshkey \
     | jq -r '.private_key' > ~/.ssh/id_ed25519 \
-    && chmod 600 ~/.ssh/id_ed25519
+    && chmod 600 $HOME/.ssh/id_ed25519
 
-echo ${DOCKER_CONFIG} > /root/.docker/config.json
-dockerd --data-root /workspaces/.docker > /var/log/dockerd.log 2>&1 &
+sudo sh -c "echo $DOCKER_CONFIG_JSON > /root/.docker/config.json"
+sudo dockerd --data-root /workspaces/.docker > ~/.logs/dockerd.log 2>&1 &
 sleep 2
 
 if [ ! -d "./code" ] ; then
@@ -23,9 +23,10 @@ node /scripts/forward-port.js ${CONFIG_PATH}
 devcontainer up ${DC_ARG_REBUILD} --workspace-folder=./code \
     --mount=type=bind,source=/tmp/code_x64,target=/usr/bin/code \
     --mount=type=bind,source=/var/run/docker.sock,target=/var/run/docker.sock \
-    --mount=type=bind,source=/workspaces/.vscode-cli,target=/root/.vscode \
-    --mount=type=bind,source=/root/.ssh/id_ed25519,target=/root/.ssh/id_ed25519
+    --mount=type=bind,source=/workspaces/.vscode-cli,target=/workspaces/.vscode-cli \
+    --mount=type=bind,source=$HOME/.ssh/id_ed25519,target=/tmp/.ssh/id_ed25519
 
 mv ${CONFIG_PATH}.bak ${CONFIG_PATH}
 
-devcontainer exec --workspace-folder=./code code serve-web --host 0.0.0.0 $@ --without-connection-token --accept-server-license-terms > /var/log/vscode-web.log 2>&1 &
+devcontainer exec --workspace-folder=./code bash -c 'ln -fs /workspaces/.vscode-cli $HOME/ && ln -fs /tmp/.ssh $HOME/ && chown $(whomai) -R ~/.ssh'
+devcontainer exec --workspace-folder=./code code serve-web --host 0.0.0.0 $@ --without-connection-token --accept-server-license-terms > ~/.logs/vscode-web.log 2>&1 &
