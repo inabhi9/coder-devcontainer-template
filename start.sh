8c8a0bdade3be28e7ed315c35dc6c2b04b47b2b8
Abhinav <kotak@ldopa.in>
2024-07-09 08:46:05 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-mkdir -p /workspaces/.vscode-cli /workspaces/.docker ~/.ssh ~/.logs
+mkdir -p /workspaces/.vscode /workspaces/.docker ~/.ssh ~/.logs
 sudo mkdir -p /root/.docker
 
 # Found this api when these CODER_AGENT_* env vars are not set and executing `./coder gitssh --`
@@ -23,10 +23,11 @@ node /scripts/forward-port.js ${CONFIG_PATH}
 devcontainer up ${DC_ARG_REBUILD} --workspace-folder=./code \
     --mount=type=bind,source=/tmp/code_x64,target=/usr/bin/code \
     --mount=type=bind,source=/var/run/docker.sock,target=/var/run/docker.sock \
-    --mount=type=bind,source=/workspaces/.vscode-cli,target=/workspaces/.vscode-cli \
-    --mount=type=bind,source=$HOME/.ssh/id_ed25519,target=/tmp/.ssh/id_ed25519
+    --mount=type=bind,source=/workspaces/.vscode,target=/workspaces/.vscode \
+    --mount=type=bind,source=$HOME/.ssh,target=/tmp/.ssh
 
 mv ${CONFIG_PATH}.bak ${CONFIG_PATH}
 
-devcontainer exec --workspace-folder=./code bash -c 'ln -fs /workspaces/.vscode-cli $HOME/ && ln -fs /tmp/.ssh $HOME/ && chown $(whomai) -R ~/.ssh'
-devcontainer exec --workspace-folder=./code code serve-web --host 0.0.0.0 $@ --without-connection-token --accept-server-license-terms > ~/.logs/vscode-web.log 2>&1 &
+devcontainer exec --workspace-folder=./code bash -c 'ln -fs /workspaces/.vscode $HOME/ && ln -fs /tmp/.ssh $HOME/'
+devcontainer exec --workspace-folder=./code bash -c 'chown $(whoami) -R $HOME/.ssh'
+devcontainer exec --workspace-folder=./code code serve-web --host 0.0.0.0 $@ --server-data-dir /workspaces/.vscode/server --without-connection-token --accept-server-license-terms > ~/.logs/vscode-web.log 2>&1 &
