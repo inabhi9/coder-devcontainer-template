b19ce994b8883ce47ff10c38d5122d7981d01151
Abhinav <kotak@ldopa.in>
2025-11-14 12:18:29 +0100

    Update

--- Dockerfile
+++ Dockerfile
@@ -1,27 +1,15 @@
-FROM node:22-bookworm-slim
-
-RUN apt update && apt install -y curl jq sudo \
-    && rm -rf /var/lib/apt/lists/*
-
-RUN curl -fsSL https://get.docker.com -o get-docker.sh \
-    && sh get-docker.sh
+FROM codercom/enterprise-node:ubuntu
 
 RUN curl -Lk 'https://code.visualstudio.com/sha/download?build=stable&os=cli-alpine-x64' -o /tmp/vscode_cli.tar.gz \
     && tar -xf /tmp/vscode_cli.tar.gz -C /tmp \
     && mv /tmp/code /tmp/code_x64 \
     && rm -fr /tmp/vscode_cli.tar.gz
 
-RUN npm install -g @devcontainers/cli json5 yaml
+RUN sudo npm install -g @devcontainers/cli json5 yaml
 
-ENV NODE_PATH=/usr/local/lib/node_modules
+ENV NODE_PATH=/usr/lib/node_modules
 
 COPY start.sh /scripts/start.sh
 COPY forward-port.js /scripts/forward-port.js
 
-RUN echo '%sudo ALL=(ALL) NOPASSWD:ALL' >> /etc/sudoers
-RUN adduser coder && usermod -aG docker,sudo coder
-
-USER coder
-
-WORKDIR /workspaces
 RUN mkdir -p ~/.ssh && printf "Host *\n\tStrictHostKeyChecking no\n" > ~/.ssh/config
--- start.sh
+++ start.sh
@@ -1,33 +1,35 @@
 #!/bin/bash
 
-mkdir -p /workspaces/.vscode /workspaces/.docker ~/.ssh ~/.logs
-sudo mkdir -p /root/.docker
+
+mkdir -p $HOME/.vscode $HOME/.docker/data ~/.ssh ~/.logs
 
 # Found this api when these CODER_AGENT_* env vars are not set and executing `./coder gitssh --`
 curl -s -H "Coder-Session-Token: $CODER_AGENT_TOKEN" ${CODER_AGENT_URL}api/v2/workspaceagents/me/gitsshkey \
     | jq -r '.private_key' > ~/.ssh/id_ed25519 \
     && chmod 600 $HOME/.ssh/id_ed25519
 
-sudo sh -c "echo $DOCKER_CONFIG_JSON > /root/.docker/config.json"
-sudo dockerd --data-root /workspaces/.docker > ~/.logs/dockerd.log 2>&1 &
+echo $DOCKER_CONFIG_JSON > $HOME/.docker/config.json
+sudo dockerd --data-root $HOME/.docker/data > ~/.logs/dockerd.log 2>&1 &
 sleep 2
 
-if [ ! -d "./code" ] ; then
-    git clone ${GIT_URL} ./code
+if [ ! -d "$HOME/code" ] ; then
+    git clone ${GIT_URL} $HOME/code
 fi
 
-CONFIG_PATH=./code/.devcontainer/devcontainer.json
+CONFIG_PATH=$HOME/code/.devcontainer/devcontainer.json
 cp ${CONFIG_PATH} ${CONFIG_PATH}.bak
 node /scripts/forward-port.js ${CONFIG_PATH}
 
-devcontainer up ${DC_ARG_REBUILD} --workspace-folder=./code \
+devcontainer up ${DC_ARG_REBUILD} --workspace-folder=$HOME/code \
     --mount=type=bind,source=/tmp/code_x64,target=/usr/bin/code \
     --mount=type=bind,source=/var/run/docker.sock,target=/var/run/docker.sock \
-    --mount=type=bind,source=/workspaces/.vscode,target=/workspaces/.vscode \
+    --mount=type=bind,source=$HOME/.vscode,target=/workspaces/.vscode \
     --mount=type=bind,source=$HOME/.ssh,target=/tmp/.ssh
 
+sleep 1
+echo "Devcontainer up successful"
 mv ${CONFIG_PATH}.bak ${CONFIG_PATH}
 
-devcontainer exec --workspace-folder=./code bash -c 'ln -fs /workspaces/.vscode $HOME/ && ln -fs /tmp/.ssh $HOME/'
-devcontainer exec --workspace-folder=./code bash -c 'chown $(whoami) -R $HOME/.ssh'
-devcontainer exec --workspace-folder=./code code serve-web --host 0.0.0.0 $@ --server-data-dir /workspaces/.vscode/server --without-connection-token --accept-server-license-terms > ~/.logs/vscode-web.log 2>&1 &
+devcontainer exec --workspace-folder=$HOME/code bash -c 'ln -fs /workspaces/.vscode $HOME/ && ln -fs /tmp/.ssh $HOME/'
+devcontainer exec --workspace-folder=$HOME/code bash -c 'chown $(whoami) -R $HOME/.ssh'
+devcontainer exec --workspace-folder=$HOME/code code serve-web --host 0.0.0.0 $@ --server-data-dir /workspaces/.vscode/server --without-connection-token --accept-server-license-terms > ~/.logs/vscode-web.log 2>&1 &
