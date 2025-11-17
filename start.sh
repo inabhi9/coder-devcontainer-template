31fab1f01e4c2c4babd2b73562160df32fdfca80
Abhinav <kotak@ldopa.in>
2025-11-17 20:09:55 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -17,9 +17,7 @@ if [ ! -d "$HOME/code" ] ; then
      clone ${GIT_URL} $HOME/code
 fi
 
-DEVC_CONF=/tmp/devcontainer_repo/.devcontainer/devcontainer.json
-DEVC_CONF_OVERRIDE=$(if [ -f $DEVC_CONF ]; then echo "--override-config $DEVC_CONF"; fi)
-DEVC_OUTPUT=$(devcontainer up ${DC_ARG_REBUILD} $DEVC_CONF_OVERRIDE --workspace-folder=$HOME/code \
+DEVC_OUTPUT=$(devcontainer up ${DC_ARG_REBUILD} --workspace-folder=$HOME/code \
     --mount=type=bind,source=/tmp/code_x64,target=/usr/bin/code \
     --mount=type=bind,source=/var/run/docker.sock,target=/var/run/docker.sock \
     --mount=type=bind,source=$HOME/.vscode,target=/workspaces/.vscode \
@@ -39,6 +37,6 @@ CONTAINER_IP=$(docker inspect -f '{{range.NetworkSettings.Networks}}{{.IPAddress
 nohup socat TCP-LISTEN:13338,fork,reuseaddr TCP:$CONTAINER_IP:13338 > /dev/null 2>&1 &
 echo "Devcontainer up successful"
 
-devcontainer exec --workspace-folder=$HOME/code $DEVC_CONF_OVERRIDE bash -c 'ln -fs /workspaces/.vscode $HOME/ && ln -fs /tmp/.ssh $HOME/'
-devcontainer exec --workspace-folder=$HOME/code $DEVC_CONF_OVERRIDE bash -c 'chown $(whoami) -R $HOME/.ssh'
-devcontainer exec --workspace-folder=$HOME/code $DEVC_CONF_OVERRIDE code serve-web --host 0.0.0.0 $@ --server-data-dir /workspaces/.vscode/server --without-connection-token --accept-server-license-terms > ~/.logs/vscode-web.log 2>&1 &
+devcontainer exec --workspace-folder=$HOME/code bash -c 'ln -fs /workspaces/.vscode $HOME/ && ln -fs /tmp/.ssh $HOME/'
+devcontainer exec --workspace-folder=$HOME/code bash -c 'chown $(whoami) -R $HOME/.ssh'
+devcontainer exec --workspace-folder=$HOME/code code serve-web --host 0.0.0.0 $@ --server-data-dir /workspaces/.vscode/server --without-connection-token --accept-server-license-terms > ~/.logs/vscode-web.log 2>&1 &
